876ecdbc8bef80e94dc01d60a209a06b080a4f10
Dylan Groos <hawkins.groos@gmail.com>
2022-01-05 15:26:28 -0500

    errors with node/npm versioning resolved between linux/unix machines

--- app/models/game.js
+++ app/models/game.js
@@ -3,28 +3,43 @@ const baseUrl = "https://statsapi.web.nhl.com/api/v1";
 
 
 module.exports = function(app){
-    app.get('/api/games', function(req, res){
-        getGames('2018-01-09').then(data => {
+    // get all games today
+    app.get('/api/games/', function(req, res){
+        getGames().then(data => {
             res.type('json').send(JSON.stringify(data, null, 2) + '\n');
         });
     });
 
-    app.get('/api/game/:id', function(req, res){
-        getGames('test', req.params.id).then(data => {
+    // get all games on a single date
+    app.get('/api/games/:date', function(req, res){
+        getGames(req, req.params.date).then(data => {
             res.type('json').send(JSON.stringify(data, null, 2) + '\n');
-        })
+        });
+    });
+
+    //get a boxscore by id
+    app.get('/api/game/:id', function(req, res){
+        getGames('2018-01-09', req.params.id).then(data => {
+            res.type('json').send(JSON.stringify(data, null, 2) + '\n');
+        });
     });
 }
 
-function getGames(date, id=false) {
-    if (id){
+function getGames(date=false, id=false) {
+    if (id){    
         return getEndpoint(`/game/${id}/boxscore`).then(data => {
             console.log(data);
             return data;
         })
         .catch(err => console.log(err));
-    } else {
+    } else if (date) {
         return getEndpoint(`/schedule?date=${date}`).then(data => {
+            console.log(data);
+            return data;
+        })
+        .catch(err => console.log(err));
+    } else {
+        return getEndpoint(`/schedule`).then(data => {
             console.log(data);
             return data;
         })
